3369f5792843544568fc561b8ccce396b8e6777f
interveneMachine <bartek.ciesla0@gmail.com>
2026-05-05 22:43:03 +0200

    added pass for building SymbolTable

--- Program.cs
+++ Program.cs
@@ -4,13 +4,17 @@ class Program
 {   
     static void Main(String[] args)
     {
-        string Path = "examples/example01.awk";
+        string Path = "examples/example_function.awk";
         ICharStream inputStream = CharStreams.fromPath(Path);
         AwkLexer lexer = new AwkLexer(inputStream);
         CommonTokenStream stream = new CommonTokenStream(lexer);
         AwkParser parser = new AwkParser(stream);
 
         AwkParser.ProgramContext context = parser.program();
+
+        var table = new AwkToC.Semantic.SymbolTableBuilder().Build(context);
+        
+        table.All().ToList().ForEach(v => Console.WriteLine(v.Name + "\t" + v.Scope + "\t" + v.Type));
         Console.WriteLine("Parse tree: " + context.ToStringTree(parser));
     }
 }
