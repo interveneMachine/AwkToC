869162fe2f52f777751809de2323ac8afcc0a944
interveneMachine <bartek.ciesla0@gmail.com>
2026-06-09 20:41:15 +0200

    added list of restricted c names to UsedCNames hashset in SymbolTable

--- AwkToC/semantic/SymbolTable.cs
+++ AwkToC/semantic/SymbolTable.cs
@@ -1,10 +1,8 @@
-using System.Transactions;
-
 namespace AwkToC.Semantic;
 
 public class SymbolTable
 {
-    private HashSet<string> UsedCNames = new();
+    private HashSet<string> UsedCNames;
     private readonly Dictionary<string, Symbol> symbols = new();
     private readonly Dictionary<string, List<CSymbol>> TmpSymbols = new();
     private int temporaryCounter = 0;
@@ -14,6 +12,18 @@ public class SymbolTable
 
     public SymbolTable()
     {
+        UsedCNames = new HashSet<string>([
+            "isBegin", "buffer", "file", "argc", "argv", "main", "stderr", "stdout",
+            "AwkValueType", "AwkValue", "AWK_UNDEFINED", "AWK_NUMBER", "AWK_STRING", "Fields", "ArrayEntry", "Array", "ArrayIterator",
+            "array_init", "array_get_value", "array_set_value", "array_delete_value", "array_delete", "array_free", "arrayiterator_init", "arrayiterator_next",
+            "remove_newline", "fields_string", "fields_get", "fields_assign", "fields_free",
+            "awk_undefined", "awk_number", "awk_string", "awk_copy", "awk_match", "awk_free", "awk_to_number", "awk_to_string", "awk_is_truthy", "awk_to_int",
+            "awk_add", "awk_sub", "awk_mul", "awk_div", "awk_mod", "awk_pow", "awk_unary_plus", "awk_unary_minus", "awk_eq", "awn_ne", "awk_lt", "awk_le", "awk_gt",
+            "awk_ge", "awk_not", "awk_concat", "awk_concat_array_arg", "awk_set_default_predefined", "awk_print_value", "awk_print_values", "awk_strdup",
+            "awk_output_redirection_write", "awk_output_redirection_append", "awk_output_redirection_pipe",
+            "NULL", "int", "long", "float", "double", "FILE", "char", "size_t", "const", "void", "struct", "extern", "static", "typedef", "return", "regmatch_t", "regex_t",
+            "malloc", "free", "strcat", "strlen", "strcpy", "strncpy", "fprintf", "exit", "regfree", "regexec", "regcomp", "fopen", "fclose",
+        ]);
         Add(new Symbol
         {
             Name = "NR",
